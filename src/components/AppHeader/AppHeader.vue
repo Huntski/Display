ce454f6c44862ee47236f261068dfa7a6f31e39f
Huntski <wieberan@hotmail.nl>
2022-05-22 22:27:29 +0200

    Add: Reajust front layout

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex items-center">
+    <div>
+      <h1 class="text-3xl font-extrabold">Good morning! 👋</h1>
+      <p>Another great day to ‘study’.</p>
+    </div>
+
+
+    <div class="ml-auto">
+      <SearchInput v-model:value="searchQuery" @keydown.enter="searchMedia" />
+    </div>
+  </div>
+</template>
